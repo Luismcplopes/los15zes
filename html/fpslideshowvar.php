e77f710d153688e20d05c3ddd6e084ab9472060f
Paulo <60433500+PFCarvalho@users.noreply.github.com>
2020-02-11 16:23:34 +0000

    Update fpslideshowvar.php

--- html/fpslideshowvar.php
+++ html/fpslideshowvar.php
@@ -1,13 +1,13 @@
 <?
 Header("content-type: application/x-javascript");
 
-function returnimages($dirname=getcwd()) {
+function returnimages($dirname=".") {
    $pattern="\.(jpg|jpeg|png|gif|bmp)$";
    $files = array();
    $curimage=0;
    if($handle = opendir($dirname)) {
        while(false !== ($file = readdir($handle))){
-               if(eregi($pattern, $file)){
+               if(preg_match ($pattern, $file)){
 		 $filedate=date ("M d, Y H:i:s", filemtime($file));
                  echo "		[$curimage, \"$file\", \"$filedate\"],\n";
                  $curimage++;
